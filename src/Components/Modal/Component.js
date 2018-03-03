ae8df2cf8fe14f6aee6925fdc084744526d1ef6b
Jeff Andolora <jeeeeefff@gmail.com>
2018-03-03 18:56:12 -0500

    Bring the modal up to speed with the rest of the styling

--- src/Components/Modal/Component.js
+++ src/Components/Modal/Component.js
@@ -1,11 +1,18 @@
 import React from 'react'
+import Button from '../Button/Button'
 
-export default ({problem, onDeleteClick}) => {
+export default ({problem, section, onDeleteClick, onCancelClick}) => {
   if(problem) {
     return (
       <div id="modal">
-        <h3>{problem.text}</h3>
-        <div onClick={onDeleteClick}>Solved!</div>
+        <h3 className="modal-header">{problem.text}</h3>
+        <div className="modal-content">Section: {section.name}</div>
+        <div className="modal-action-left">
+          <Button onButtonClick={onCancelClick}>Cancel</Button>
+        </div>
+        <div className="modal-action-right">
+          <Button onButtonClick={onDeleteClick}>Solved!</Button>
+        </div>
       </div>
     )
   } else {
--- src/Components/Modal/Modal.js
+++ src/Components/Modal/Modal.js
@@ -1,15 +1,18 @@
 import { connect } from 'react-redux'
 import Component from './Component'
-import { deleteModalProblem } from '../../actions'
+import { deleteModalProblem, openProblemModal } from '../../actions'
 require('./Modal.css')
 
 const mapStateToProps = (state, ownProps) => ({
   problem: state.problem.byId.find(
-    it => it.id === state.modalProblem)
+    it => it.id === state.modalProblem),
+  section: state.section.byId.find(
+    it => it.problems.includes(state.modalProblem))
 })
 
 const mapDispatchToProps = (dispatch, ownProps) => ({
-  onDeleteClick: () => dispatch(deleteModalProblem())
+  onDeleteClick: () => dispatch(deleteModalProblem()),
+  onCancelClick: () => dispatch(openProblemModal(''))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(Component)
