1ca229b765db97d2975dc0e8f6c068340cde0176
Jeff Andolora <jeeeeefff@gmail.com>
2018-04-28 15:32:20 -0400

    Finally happy with the look/feel... for now.

--- src/Components/Section/Component.js
+++ src/Components/Section/Component.js
@@ -6,9 +6,9 @@ export default ({section, problems, onProblemClick, onSectionClick}) => (
   <div className="section">
     <h2 className="section-header">
       {section.name}
-      <img class="section-hamburger" alt="Edit Section Menu"
-          onClick={() => onSectionClick(section.id)} src={img} />
     </h2>
+    <img className="section-hamburger" alt="Edit Section Menu"
+          onClick={() => onSectionClick(section.id)} src={img} />
     <div className="section-problems">
       {problems.map(it => (
         <div key={it.id} className="section-problem-button">
